58ca3ae557cac44fe1af8e0b552ad969ab6aa374
Eshqol <eshqoldev@gmail.com>
2022-06-11 15:03:16 +0300

    Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Question from "./Question/question";
+import Box from "./Box/Box";
+import * as firebase from './firebase';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(<div/>);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+
+        const temp = []
+
+        async function newQuestions() {
+
+        const res = await firebase.newQuestions()
+
+        for (const key in res) {
+            const data = res[key]
+            const unpack = [data.title, data.description, data.user, data.time]
+            const [title, description, user, time] = unpack
+
+            temp.push(
+                <div className={'question'} onClick={() => openQuestion(unpack)}>
+                    <Box title={title} user={user} time={time} answers={['a', 'b', 'c']}/>
+                </div>
+            )
+        }
+
+        setQuestions(temp)
+
+
+        }
+        newQuestions()
+
+    }, [])
+
+
+    function openQuestion(args) {
+
+        const [title, description, user, time] = args
+
+        setCurrentQuestion(<Question setShow={setShow} title={title} user={user}/>)
+        setShow(true)
+
+    }
+
+
+    return (
+        <div style={{background: 'black', position: 'absolute', width: '100%', height: '100%'}} className="App">
+            {show && currentQuestion}
+            <div className={'all-questions'}>
+                {questions}
+            </div>
+        </div>
+    )
+}
